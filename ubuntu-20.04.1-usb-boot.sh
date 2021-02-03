4672684aa5e405e44fe12576332f362e5de618b0
Wouter Stemgée <wouterstemgee@gmail.com>
2021-02-03 21:59:25 +0100

    ubuntu usb boot

--- ubuntu-20.04.1-usb-boot.sh
+++ ubuntu-20.04.1-usb-boot.sh
@@ -3,13 +3,13 @@
 # More details: https://ubuntu.com/tutorials/how-to-install-ubuntu-desktop-on-raspberry-pi-4#4-optional-usb-boot
 
 # Extract the current bootloader configuration to a text file
+apt install rpi-eeprom
 vcgencmd bootloader_config > bootconf.txt
 
 # Set the BOOT_ORDER option to 0xf41 (meaning attempt SD card, then USB mass-storage device, then repeat)
 sed -i -e '/^BOOT_ORDER=/ s/=.*$/=0xf41/' bootconf.txt
 
 # Generate a copy of the EEPROM with the update configuration
-apt install rpi-eeprom
 rpi-eeprom-config --out pieeprom-new.bin --config bootconf.txt /lib/firmware/raspberrypi/bootloader/critical/pieeprom-2020-09-03.bin
 
 # Set the system to flash the new EEPROM firmware on the next boot
