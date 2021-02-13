ddcc1448de5aafca18caad3a3c559a66b950cb8f
Wouter Stemgée <wouterstemgee@gmail.com>
2021-02-13 10:48:21 +0100

    [picluster] Added fabfile.py

---
+++ picluster/fabric/fabfile.py
@@ -0,0 +1,64 @@
+from fabric.api import *
+from fabric.colors import green
+
+env.skip_bad_hosts=True
+
+
+env.hosts = [
+    'ubuntu@control01.picluster', 
+    'ubuntu@worker01.picluster', 
+    'ubuntu@worker02.picluster'
+            ]
+ 
+# All Raspberrys have the same password 
+env.password = ""
+ 
+@parallel
+def check():
+    # check host type
+    host_type()
+ 
+    # Check diskspace
+    diskspace()
+ 
+@parallel
+def cmd(command):
+     if run(command).failed:
+         sudo(command)
+
+@parallel
+def sudocmd(command):
+    sudo(command)
+
+@serial
+def scmd(serialcommand):
+    if run(serialcommand).failed:
+        sudo(serialcommand)
+
+@parallel
+def allshutdown():
+    sudo("shutdown -t 1") # shutdown in 1 min
+ 
+@parallel
+def allreboot():
+    sudo("shutdown -r -t 1") # reboot in 1 min
+ 
+@parallel
+def allcancel():
+    sudo("shutdown -c") # cancel the last shutdown command
+ 
+@parallel
+def alldate():
+    run("date +\"Date : %d/%m/%Y Time : %H.%M.%S\"") # show Time and Date
+
+@parallel
+def file_send(localpath, remotepath):
+    put(localpath, remotepath, use_sudo=True)
+
+@parallel
+def file_get(remotepath, localpath):
+    get(remotepath, localpath + "." + env.host)
+
+@parallel
+def cputemp():
+    run("cat /sys/class/thermal/thermal_zone0/temp")
